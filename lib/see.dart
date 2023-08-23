14053321281e0cb29bed3ef40aba315508672a68
aditya-gupta-dev <142041364+aditya-gupta-dev@users.noreply.github.com>
2023-08-23 22:05:38 +0530

    Added listFiles method

--- lib/see.dart
+++ lib/see.dart
@@ -1,3 +1,13 @@
-int calculate() {
-  return 6 * 7;
+import 'dart:io';
+
+List<File> listFiles(Directory dir) {
+  List<File> fileList = [];
+
+  dir.listSync(recursive: true).forEach((FileSystemEntity entity) {
+    if (entity is File) {
+      fileList.add(entity);
+    }
+  });
+
+  return fileList;
 }

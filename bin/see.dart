f0588050b0718e76cecafc25edaaa1366955d59e
aditya-gupta-dev <142041364+aditya-gupta-dev@users.noreply.github.com>
2023-08-23 22:06:21 +0530

    Cleared everything

--- bin/see.dart
+++ bin/see.dart
@@ -1,17 +1,3 @@
-import 'dart:io';
-
-List<File> listFiles(Directory dir) {
-  List<File> fileList = [];
-
-  dir.listSync(recursive: true).forEach((FileSystemEntity entity) {
-    if (entity is File) {
-      fileList.add(entity);
-    }
-  });
-
-  return fileList;
-}
-
 void main(List<String> args) {
-  if (args.isNotEmpty) {}
+  
 }

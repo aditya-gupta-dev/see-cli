ba7b09eb57f46179c412536f44d4529ab7825ff9
aditya-gupta-dev <142041364+aditya-gupta-dev@users.noreply.github.com>
2023-08-24 16:08:45 +0530

    Added function to get only two directories

--- lib/see.dart
+++ lib/see.dart
@@ -11,3 +11,17 @@ List<File> listFiles(Directory dir) {
 
   return fileList;
 }
+
+List<Directory> getOnlyTwoDirectories(Directory directory) {
+  List<Directory> directories = [];
+  List<FileSystemEntity> entities = directory.listSync();
+  for (FileSystemEntity entity in entities) {
+    if (directories.length == 2) {
+      return directories;
+    }
+    if (entity is Directory) {
+      directories.add(entity);
+    }
+  }
+  return directories;
+}

c106aef8c02a8280ed901b9c7f01fdfecd93002e
aditya-gupta-dev <142041364+aditya-gupta-dev@users.noreply.github.com>
2023-08-24 16:17:37 +0530

    Update see.dart

--- bin/see.dart
+++ bin/see.dart
@@ -1,3 +1,12 @@
+import 'dart:io';
+import 'package:see/see.dart';
+
 void main(List<String> args) {
-  
+  String path = "H:\\darting\\see\\lib";
+
+  List<Directory> directories = getOnlyTwoDirectories(Directory(path));
+  if (directories.isEmpty) {
+    print("You need to have atleast two directories...");
+    return;
+  }
 }

64954f7278afb90caec2f9f27fc9fa5cce0bd5e6
aditya-gupta-dev <142041364+aditya-gupta-dev@users.noreply.github.com>
2023-08-24 20:33:08 +0530

    Update see.dart

--- bin/see.dart
+++ bin/see.dart
@@ -1,12 +1,33 @@
 import 'dart:io';
 import 'package:see/see.dart';
+import 'package:path/path.dart';
 
-void main(List<String> args) {
-  String path = "H:\\darting\\see\\lib";
+void main(List<String> args) async {
+  String path = "H:\\darting\\remo";
 
   List<Directory> directories = getOnlyTwoDirectories(Directory(path));
   if (directories.isEmpty) {
     print("You need to have atleast two directories...");
     return;
+  } else {
+    String firstDirName = basename(directories[0].path);
+    String secondDirName = basename(directories[1].path);
+
+    print("Comparing $firstDirName with $secondDirName....");
+
+    List<Future<List<File>>> futures = [
+      listFiles(directories[0]),
+      listFiles(directories[1])
+    ];
+
+    List<List<File>> files = await Future.wait(futures);
+
+    if (!isEqualNumberOfFiles(files[0], files[1])) {
+      print(
+          "$firstDirName contains ${files[0].length} files and $secondDirName contains ${files[1].length} files");
+    } else {
+      print("$firstDirName and $secondDirName contain equal number of files");
+      compareDirectories(files[0], files[1]);
+    }
   }
 }
